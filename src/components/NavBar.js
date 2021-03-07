3866073458c7d1393f21aa5ade0c6a36570b2629
David Bril <davidbril91@gmail.com>
2021-03-07 20:50:37 -0300

    second commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+function NavBar() {
+  return (
+    <div className="App">
+      <ul>
+        <li><a href="#home">Mi menu</a></li>
+        <li><a href="#news">Compras</a></li>
+        <li><a href="#contact">Contacto</a></li>
+        <li><a href="#about">About</a></li>
+      </ul>
+    </div>
+  );
+}
+
+
+export default NavBar;
