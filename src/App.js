368d57f9e2df2510c2afcac45073f434500c7a17
Davebril <68717358+Davebril@users.noreply.github.com>
2021-03-15 19:42:41 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import NavBar from './components/NavBar';
 import CartWidget from './components/CartWidget';
-import AppFunction from './components/AppFunction';
 import ItemListContainer from './components/ItemListContainer';
 import CounterApp from './components/CounterApp';
 
@@ -9,7 +8,6 @@ function App() {
   return <>
          <NavBar/>
         <CartWidget/>
-        <AppFunction/>
         <ItemListContainer greeting={{ name: 'querido usuario!' }} />
         <CounterApp/>
         </>
